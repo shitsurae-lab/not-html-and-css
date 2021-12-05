a014bd61a89b9cfd50a1741e57b90078e3ea1bdb
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-05 21:04:29 +0900

    fontawesome

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,15 @@
 //import Default from './drawer.js';
 import './drawer.js';
 
-/*
+/* *****
  *CSS
- */
+ ***** */
 //normalize.css
 import 'normalize.css';
 import '../css/style.scss';
+
+//font awesome
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
