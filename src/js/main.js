9df0d939ce2b89ec979397682a53e431097f6f71
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-06 13:15:25 +0900

    import fontawesome from '@fortawesome/fontawesome'

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,10 @@ import 'normalize.css';
 import '../css/style.scss';
 
 //font awesome
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
+// Font-awesome 5 doesn't work(https://bit.ly/3GdFxAi)
+import fontawesome from '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import regular from '@fortawesome/fontawesome-free/scss/regular.scss';
+import solid from '@fortawesome/fontawesome-free/scss/solid.scss';
+import brands from '@fortawesome/fontawesome-free/scss/brands.scss';
+
+fontawesome.library.add(solid, regular, brands);
