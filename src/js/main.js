f29efca6642c56783ea25fc7b3f1e495b826a78d
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-09 20:59:00 +0900

    add import './script.js'(define jq in script.js)

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,11 @@
  *JavaScript
  */
 
+import './script.js';
+
 //original drawer.js
 //import Default from './drawer.js';
 import './drawer.js';
-
-//jQueryの読み込み
-// import jQuery from 'jquery';
-//ドルマークに参照を代入(慣習的な $ を使うため)
-// const $ = jQuery;
-import './jquery-3.6.0.min.js';
-// not-text.js
-import './script.js';
-// import './original.js';
 
 /* *****
  *CSS
