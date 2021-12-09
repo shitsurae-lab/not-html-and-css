c1251e147fbfa6499e1a4c5a725cb146c4b26728
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-09 21:02:07 +0900

    Delete original.js & modify script.js(include jQ)

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,26 @@
-{
-  'use strict'
+'use strict';
 
+import jQuery from 'jquery';
+
+//$マークに参照(jQuery)を代入
+const $ = jQuery;
+
+$(function () {
   $('.navbtn').on('click', (event) => {
-    event.preventDefault()
-    $e = $(event.currentTarget)
-    if($e.hasClass('close')) {
+    event.preventDefault();
+    // $e = $(event.currentTarget);
+    if ($('.navbtn').hasClass('close')) {
       // メニューが開いている状態から閉まる状態へ
-      $('.header-nav').slideUp()
-      $e.removeClass('close')
-      $('.header-nav').addClass('collapse')
+      $('.header-nav').slideUp();
+      $('.navbtn').removeClass('close');
+      $('.header-nav').addClass('collapse');
     } else {
       // メニューが閉じている状態から開く状態へ
-      $('.header-nav').slideDown()
-      $e.addClass('close')
-      $('.header-nav').removeClass('collapse')
+      $('.header-nav').slideDown();
+      $('.navbtn').addClass('close');
+      $('.header-nav').removeClass('collapse');
     }
-  })
+  });
 
-}
+  console.log('jQueryは動いてます');
+});
