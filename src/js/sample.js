a5a532dba0c7a8385b496e576c5ac184ed3bb282
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-14 14:53:16 +0900

    add js (conditional branch) by URL

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,4 +1,9 @@
-alert('Hello World!');
-
-const url = location.url;
-console.log(url);
+const url = location.href;
+const target = document.querySelector('body');
+if (url === 'http://localhost:8080/about.html') {
+  target.classList.add('sample');
+} else if (url === 'http://localhost:8080/post.html') {
+  target.classList.add('post');
+} else {
+  target.classList.add('home');
+}
