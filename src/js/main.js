c5e23207165c481de913ad9bab7022f614506b39
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-14 12:09:44 +0900

    'fontawesome-free/scss/' → 'fontawesome-free/js/'

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import './script.js';
 //original drawer.js
 //import Default from './drawer.js';
 import './drawer.js';
+import './sample.js';
 
 /* *****
  *CSS
@@ -17,9 +18,14 @@ import '../css/style.scss';
 
 //font awesome
 // Font-awesome 5 doesn't work(https://bit.ly/3GdFxAi)
-import fontawesome from '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-import regular from '@fortawesome/fontawesome-free/scss/regular.scss';
-import solid from '@fortawesome/fontawesome-free/scss/solid.scss';
-import brands from '@fortawesome/fontawesome-free/scss/brands.scss';
+// import fontawesome from '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+// import regular from '@fortawesome/fontawesome-free/scss/regular.scss';
+// import solid from '@fortawesome/fontawesome-free/scss/solid.scss';
+// import brands from '@fortawesome/fontawesome-free/scss/brands.scss';
 
-fontawesome.library.add(solid, regular, brands);
+// fontawesome.library.add(solid, regular, brands);
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
