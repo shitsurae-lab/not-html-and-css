8c3de84db47cf5aa06d354ded8e90d96586ed067
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-18 16:19:03 +0900

    define tagList & tagList.querySelector .remove

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,3 +1,4 @@
+//URLで分岐、bodyにクラス付与
 const url = location.href;
 const target = document.querySelector('body');
 if (url === 'http://localhost:8080/about.html') {
@@ -7,3 +8,8 @@ if (url === 'http://localhost:8080/about.html') {
 } else {
   target.classList.add('home');
 }
+
+//カード型レイアウトのタグ(HTML)を一部削除
+const tagList = document.querySelector('.bl_latest_item:nth-child(2)');
+
+tagList.querySelector('.bl_latest_text span:nth-child(2)').remove();
