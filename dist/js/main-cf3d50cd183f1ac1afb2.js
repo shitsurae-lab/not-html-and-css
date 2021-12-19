8bff765d7d6569ea25253fad5de964fec929dc70
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-19 20:24:12 +0900

    Change hash value(dist/*.css, *.css.map, *.js, *.html)

--- dist/js/main-cf3d50cd183f1ac1afb2.js
+++ dist/js/main-cf3d50cd183f1ac1afb2.js
@@ -15472,12 +15472,12 @@ if (href === 'http://localhost:8080/about.html') {
 //URLで分岐、bodyにクラス付与
 const url = location.href;
 const target = document.querySelector('body');
-if (url === 'http://localhost:8080/about.html') {
+if (url.includes('/about.html')) {
   target.classList.add('sample');
-} else if (url === 'http://localhost:8080/post.html') {
+} else if (url.includes('/post.html')) {
   target.classList.add('post');
-} else if (url === 'http://localhost:8080/sidebar-post.html') {
-  target.classList.add('sidebar-post');
+} else if (url.includes('/sidebar-post.html')) {
+  target.classList.add('sidebar-post', 'post');
 } else {
   target.classList.add('home');
 }
@@ -15485,7 +15485,7 @@ if (url === 'http://localhost:8080/about.html') {
 //カード型レイアウトのタグ(HTML)を一部削除
 const tagList = document.querySelector('.bl_latest_item:nth-child(2)');
 
-tagList.querySelector('.bl_latest_text span:nth-child(2)').remove();
+// tagList.querySelector('.bl_latest_text span:nth-child(2)').remove();
 
 
 /***/ }),
@@ -15671,4 +15671,4 @@ __webpack_require__.r(__webpack_exports__);
 
 /******/ })()
 ;
-//# sourceMappingURL=main-bdac98d5924c744d4345.js.map
+//# sourceMappingURL=main-cf3d50cd183f1ac1afb2.js.map
