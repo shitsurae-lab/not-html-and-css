1885e6611c9ddb4e9f3ab7236b19e277ffc52138
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-12-22 21:32:55 +0900

    modify JS (match url → match part)

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,15 +1,16 @@
 //URLで分岐、bodyにクラス付与
 const url = location.href;
 const target = document.querySelector('body');
-if (url === 'http://localhost:8080/about.html') {
 
 //カード型レイアウトのタグ(HTML)を一部取得
 const tagList = document.querySelector('.bl_latest_item:nth-child(2) .bottom');
+
+if (url.includes('/about.html')) {
   target.classList.add('sample');
-} else if (url === 'http://localhost:8080/post.html') {
+} else if (url.includes('/post.html')) {
   target.classList.add('post');
-} else if (url === 'http://localhost:8080/sidebar-post.html') {
-  target.classList.add('sidebar-post');
+} else if (url.includes('/sidebar-post.html')) {
+  target.classList.add('sidebar-post', 'post');
 } else {
   target.classList.add('home');
   //カード型レイアウトのタグ(HTML)を一部削除
